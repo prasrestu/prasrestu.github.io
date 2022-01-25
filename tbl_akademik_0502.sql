21a2bf41d9a9a052615dd8637440a274cfd89cfc
Prasrestu <94103170+prasrestu@users.noreply.github.com>
2022-01-25 13:04:18 +0700

    Add files via upload

---
+++ tbl_akademik_0502.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 25, 2022 at 07:03 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_akademik_0502`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_akademik_0502`
+--
+
+CREATE TABLE `tbl_akademik_0502` (
+  `Id` int(5) NOT NULL,
+  `nim` varchar(20) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `jk` varchar(15) NOT NULL,
+  `jurusan` varchar(20) NOT NULL,
+  `alamat` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
